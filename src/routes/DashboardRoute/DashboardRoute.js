dd4f1ef9a8b53d91de8990cc3ab094504b730587
Justin Foster <jfoster91@gmail.com>
2020-10-05 12:11:30 -0400

    Dashboard link to Learn route

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 import Button from '../../components/Button/Button'
 import './DashboardRoute.css'
 
 class DashboardRoute extends Component {
+
+  handleClickStart() {
+    const { history } = this.props
+    history.push('/learn')
+  }
+
   render() {
     return (
       <div>
         <section className='dashBoard-menu'>
           <h2>Japanese</h2>
-          <Button>Start Practice!</Button>
+          <Button onClick={() => this.handleClickStart()}
+          >Start Practice!</Button>
         </section>
 
         <section className='dashBoard-userscore'>
