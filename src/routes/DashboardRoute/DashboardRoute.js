f4f9e0088bc8e49581f6dd017463f7aff071f2ae
Sonali Najera <sonalitzul@gmail.com>
2020-10-05 08:46:36 -0700

    markup for dashboard page

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import Button from '../../components/Button/Button'
+import './DashboardRoute.css'
 
 class DashboardRoute extends Component {
   render() {
     return (
-      <section>
-        implement and style me
-      </section>
+      <div>
+        <section className='dashBoard-menu'>
+          <h2>Japanese</h2>
+          <Button>Start Practice!</Button>
+        </section>
+
+        <section className='dashBoard-userscore'>
+          <p>Total Score: 11000 out of 22222 correct </p>
+        </section>
+        
+        <section className='dashBoard-course-overview'>
+          <ul>
+            <li>Hello</li>
+            <li>Goodbye</li>
+            <li>What time is it?</li>
+            <li>Help</li>
+          </ul>
+        </section>
+      </div>
     );
   }
 }
