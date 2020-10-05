eeb306157d0a95f221f03b5e88c510d53e2074e8
Justin Foster <jfoster91@gmail.com>
2020-10-05 11:51:06 -0400

    Dashboard stuff

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import './DashboardRoute.css'
 
 class DashboardRoute extends Component {
   render() {
     return (
-      <section>
-        implement and style me
+      <section className='DashboardRoute'>
+        <h2>Japanese Greetings</h2>
+        <p>Total score x out of y correct.</p>
+        <div className='WordDisplay'>
+          <h3>Words to Practice</h3>
+          {/* Need to pull list from DB */}
+          <ul>
+            <li>Hello</li>
+            <li>Goodbye</li>
+            <li>What time is it?</li>
+            <li>Thank you</li>
+            <li>Home</li>
+            <li>Help</li>
+            <li>Please</li>
+            <li>I don't speak Japanese</li>
+            <li>Dog</li>
+            <li>Cat</li>
+          </ul>
+          <button className='startButton'>Start Learning</button>
+        </div>
       </section>
     );
   }
