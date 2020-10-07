b4b6c9ffc1b8bdb0ddf18b6c8f33749b6a7dbd6a
Justin Foster <jfoster91@gmail.com>
2020-10-07 14:56:21 -0400

    Moved AnswerResults to new component

--- src/components/AnswerResult/AnswerResult.js
+++ src/components/AnswerResult/AnswerResult.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react'
 import './AnswerResult.css'
 
 class AnswerResult extends Component {
-  
+
   render() {
-    console.log(this.props)
+    let {hiddenAnswerSection, translation, userGuess, totalScore, currentWord} = this.props;
+    let submissionFeedback = this.props.isCorrect
+    ? <h3 className='greenTea'>You were correct! :D</h3>
+    : <h3 className='strawberry'>Good try, but not quite right :(</h3>;
+
     return (
-      <section className='answerResults'>
-        <h2>'Good try, but not quite right :('</h2>
-        <h1>Hello</h1>
-        <div className='feedback'>
-          <p>The correct translation for -original- was 
-          -answer- and you chose -guess-!</p>
+        <div className={`results ${hiddenAnswerSection}`}>
+          <section className='answerResults'>
+            {submissionFeedback}
+            <div className='feedback'>
+              <p className='answerResults-paragraph'>The correct translation for <span className="bold">{currentWord}</span> <br />
+              was <span className='greenTea'>{translation}</span> <br />
+              and you chose {userGuess}</p>
+            </div>
+            <button onClick={this.props.handleNextTryClick} className='submit'>Try another word.</button>
+            <div className='DisplayScore'>
+              <p >Your total score is: {totalScore}</p>
+            </div>
+          </section>
         </div>
-        <button>Try another word.</button>
-        <p>Your total score is: %correct </p>
-      </section>
-    )
+    );
   }
 }
 
