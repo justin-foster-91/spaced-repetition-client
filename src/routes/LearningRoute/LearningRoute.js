6579b03d5a7a7e2256e858d26874d391622addd3
Sonali Najera <sonalitzul@gmail.com>
2020-10-06 20:56:03 -0700

    added styling to feedback

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -16,7 +16,7 @@ class LearningRoute extends Component {
     }
   }
   
-  componentDidMount() {
+  getHead() {
     WordApiService.getHead()
       .then(res => {
         if (!res.ok) {
@@ -28,6 +28,10 @@ class LearningRoute extends Component {
         this.setState({ currentWord: res });
       })
       .catch(error => console.error(error));
+  }
+  
+  componentDidMount(){
+    return this.getHead()
   }
 
   handleSubmit = ev => {
@@ -54,24 +58,29 @@ class LearningRoute extends Component {
       didSubmit: false,
       isCorrect: null,
     })
+    return this.getHead();
   }
 
   render() {
     console.log(this.state.currentWord)
     let currentWord = this.state.currentWord ? this.state.currentWord.nextWord : '';
     let translation = this.state.didSubmit ? this.state.rightAnswer : '';
-    let userGuess = this.state.didSubmit ? this.state.userAnswer : '';
+    let userGuess = this.state.didSubmit 
+      ? <span className={this.state.isCorrect ? 'greenTea': 'strawberry'}>{this.state.userAnswer}</span>
+      : '';
     let totalScore = this.state.currentWord ? this.state.currentWord.totalScore : '';
     let correctlyAnswered = this.state.currentWord ? this.state.currentWord.wordCorrectCount : '';
     let incorrectlyAnswered = this.state.currentWord ? this.state.currentWord.wordIncorrectCount : '';
-    let submissionFeedback = this.state.isCorrect ? <h2>You were correct! :D</h2> : <h2>Good try, but not quite right :(</h2>
+    let submissionFeedback = this.state.isCorrect 
+      ? <h2 className='greenTea'>You were correct! :D</h2> 
+      : <h2 className='strawberry'>Good try, but not quite right :(</h2>;
     let hiddenSubmission = this.state.didSubmit ? 'hidden' : '';
     let hiddenAnswerSection = !this.state.didSubmit ? 'hidden' : '';
     return (
       <section className='LearningDisplay '>
         <div className='wordCard'>
-          <h3 className='LearningTitle'>Translate the word:</h3>
-          <h1 className='currentWord'>{currentWord}</h1>
+          <h2 className='LearningTitle'>Translate the word:</h2>
+          <p className='currentWord'>{currentWord}</p>
         </div>
         <form onSubmit={this.handleSubmit} >
           <fieldset className={`GuessForm ${hiddenSubmission}`}>
@@ -85,13 +94,13 @@ class LearningRoute extends Component {
         <div className={`results ${hiddenAnswerSection}`}>
           <section className='answerResults'>
             {submissionFeedback}
-            <h1>{currentWord}</h1>
             <div className='feedback'>
-              <p>The correct translation for {currentWord} was
-          {translation} and you chose {userGuess}</p>
+              <p className='answerResults-paragraph'>The correct translation for <span className="bold">{currentWord}</span> <br/>
+              was <span className='greenTea'>{translation}</span> <br/>
+              and you chose {userGuess}</p>
             </div>
             <button onClick={this.handleNextTryClick}>Try another word.</button>
-            <p>Your total score is: {totalScore} correct </p>
+            <p className='answerResults-paragraph'>Your total score is: {totalScore} correct </p>
           </section>
         </div>
 
